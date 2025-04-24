ed25b52748696bae684ad6d7ef5b40d599660ab0
David <davidmmyh@gmail.com>
2025-04-24 17:40:00 +0200

    Added: soft animation to social icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
 
       <footer className="fixed bottom-2.5 start-5 space-x-2">
         <Link
-          className="inline-block bg-primary rounded-full p-1"
+          className="inline-block bg-primary rounded-full p-1 hover:scale-110 transition-transform duration-200"
           href="https://www.facebook.com/profile.php?id=61573532942122"
           target="_blank"
           rel="noopener noreferrer"
@@ -41,7 +41,7 @@ export default function Home() {
           <Facebook className="h-5 w-5 fill-white text-transparent" />
         </Link>
         <Link
-          className="inline-block bg-red-700 text-white rounded-full p-1"
+          className="inline-block bg-red-700 text-white rounded-full p-1 hover:scale-110 transition-transform duration-200"
           href="https://www.youtube.com/@SeroFeELNooor"
           target="_blank"
           rel="noopener noreferrer"
@@ -49,7 +49,7 @@ export default function Home() {
           <Youtube className="h-5 w-5" />
         </Link>
         <Link
-          className="inline-block bg-pink-600 text-white rounded-full p-1"
+          className="inline-block bg-pink-600 text-white rounded-full p-1 hover:scale-110 transition-transform duration-200"
           href="https://www.instagram.com/sero_fe_elnoor"
           target="_blank"
           rel="noopener noreferrer"
