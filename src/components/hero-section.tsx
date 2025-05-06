67bf2c4b904c3e224907ad9d2a430400c2987c16
David <davidmmyh@gmail.com>
2025-05-06 22:04:15 +0300

    Add: daily verses

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,8 +1,20 @@
+"use client";
+
+import { useRef } from "react";
 import { InstructionsButton } from "./Instructions-button";
+import verses from "@/bible-verses.json";
 
 export function HeroSection() {
+  const ref = useRef<Date>(new Date());
+  const month = ref.current.getMonth();
+  const day = ref.current.getDate();
+  const verse = verses[month][day];
+
   return (
-    <section className="text-center py-4">
+    <section className="text-center pb-4">
+      <p className="text-wrap text-black my-4 text-xs">
+        {verse.verse} <span className="text-nowrap">({verse.reference})</span>
+      </p>
       <h1 className="hero text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl bg-gradient-to-r from-[#003092] via-[#00879e] to-[#ffab5b] text-transparent bg-clip-text">
         مساحة آمنة للمشاركة
       </h1>
