3c09339474d1da6a9df573e9489c2d4997dfb905
David <davidmmyh@gmail.com>
2025-05-09 16:51:31 +0300

    Removed: background image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,18 +32,18 @@ export default function RootLayout({
       <body className={`${notoKufiArabic.className} bg-primary/5 pb-28`}>
         <div className="relative overflow-clip">
           {/* Light Image Effect */}
-          <div className="absolute top-0 right-[-930px] sm:right-auto sm:left-1/2 sm:-translate-x-1/2 w-[2000px] h-[1150px] pointer-events-none">
+          {/* <div className="absolute top-0 right-[-930px] sm:right-auto sm:left-1/2 sm:-translate-x-1/2 w-[2000px] h-[1150px] pointer-events-none">
             <Image
-              src="/white-glow-light-effect-png1.webp"
+              src="/white-glow-light-effect-png2.webp"
               alt=""
               fill
               priority
               loading="eager"
               fetchPriority="high"
-              className="object-contain object-top opacity-20 light-image-transformation"
+              className="object-contain object-top opacity-5 light-image-transformation"
               style={{ top: -90 }}
             />
-          </div>
+          </div> */}
 
           <div className="min-h-screen max-w-7xl mx-auto relative">
             <MainLayout>{children}</MainLayout>
